362cb0e48b1b9aa0ccbf7ba824791e8df39de96a
Rzied <galaxyzied@gmail.com>
2021-12-01 00:37:07 +0100

    Portfolio

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,9 +1,35 @@
-import "./intro.scss"
+import "./intro.scss";
+import { init } from 'ityped';
+import { useEffect, useRef } from "react";
 
 export default function Intro() {
-    return (
-        <div className="intro" id="intro">
-            
+  const textRef = useRef();
+
+  useEffect(() => {
+    init(textRef.current, {
+      showCursor: true,
+      backDelay: 1500,
+      backSpeed: 60,
+      strings: ["Full Stack", "Front-End", "Back-End"]
+    })
+  }, [])
+  return (
+    <div className="intro" id="intro">
+      <div className="left">
+        <div className="imgContainer">
+          <img src="assets/8.png" alt="" />
         </div>
-    )
+      </div>
+      <div className="right">
+        <div className="wrapper">
+          <h2>Hi there </h2>
+          <h1>Zied RJEB</h1>
+          <h3>Web Developper <span ref={textRef}> </span></h3>
+        </div>
+        <a href="#portfolio">
+          <img src="assets/down.png" alt="down" />
+        </a>
+      </div>
+    </div>
+  );
 }
